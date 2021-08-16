a2b2ad87b0c5d572347941f5be72a264367cfd6e
whitehatjrafreennithin <78360402+whitehatjrafreennithin@users.noreply.github.com>
2021-08-16 18:08:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function bed_btn(){
+ window.location = "bed.html"   
+}
+
+function tv_ac_btn(){
+    window.location = "tv_ac.html"   
+   }
+
+   function desk_btn(){
+    window.location = "desk.html"   
+   }
+
+   function bottle_btn(){
+    window.location = "bottle.html"   
+   }
+   function fruit_basket_btn(){
+    window.location = "fruit_basket.html"   
+   }
